5b41a401e43f300a00ad98421cf1c7d344c98e05
Attila Kasza <9ttila.kasza@gmail.com>
2020-04-30 22:41:40 +0200

    Updates

---
+++ precache-manifest.451f6fea51e06d9efe2398bab2370e95.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28725d34b7aeb689f228304e5a7f043c",
+    "url": "/react-myreads/index.html"
+  },
+  {
+    "revision": "b2d8c2f55d8084e51b8a",
+    "url": "/react-myreads/static/css/main.68db4378.chunk.css"
+  },
+  {
+    "revision": "2caa1cf66be472940279",
+    "url": "/react-myreads/static/js/2.ee7b6d66.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-myreads/static/js/2.ee7b6d66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2d8c2f55d8084e51b8a",
+    "url": "/react-myreads/static/js/main.6d224892.chunk.js"
+  },
+  {
+    "revision": "dc55090d8862d20ff631",
+    "url": "/react-myreads/static/js/runtime-main.77134ed2.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/react-myreads/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/react-myreads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/react-myreads/static/media/arrow-drop-down.78c6f53b.svg"
+  }
+]);
